8b3ed46a57ecf07d8dff0df4994e059e54f41ff3
Npvpr <nlmnever2@gmail.com>
2023-11-09 23:42:23 +0630

    Admin basic design

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,21 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  if(!session) return;
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session.user.name}</b>
+        </h2>
 
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
-        <div>
-          <button
-            className="bg-white p-2 px-4 rounded-lg"
-            // Putting 'google' argument into signIn() will stop redirecting
-            // to google's signIn UI (better UI)
-            onClick={() => signIn('google')}
-          >
-            Login with Google
-          </button>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session.user.image} alt="User Image" className="w-6 h-6"/>
+          <span className="px-2">{session.user.name}</span>
         </div>
       </div>
-    );
-  }
-  return (
-    <>
-      <div>Signed in as {session.user.email}</div>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
+    </Layout>
   );
 }
