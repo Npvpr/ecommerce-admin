8e466c234f65284ff5084a49473473944fdfb0d6
Npvpr <nlmnever2@gmail.com>
2023-12-16 15:34:03 +0630

    finished essential features, preparing to deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,44 @@
-import clientPromise from "@/lib/mongodb"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import {Admin} from "@/models/Admin";
+import {mongooseConnect} from '@/lib/mongoose';
 
-export default NextAuth({
+async function isAdminEmail(email) {
+  mongooseConnect();
+  // console.log(email);
+  return !! (await Admin.findOne({email}));
+}
+
+export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-})
+  callbacks: {
+    session: async ({session,token,user}) => {
+      if (await isAdminEmail(session?.user?.email)) {
+        return session;
+      } else {
+        console.log('Not an Admin Email')
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!(await isAdminEmail(session?.user?.email))) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
